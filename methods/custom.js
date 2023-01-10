b4c581f84615db6c311036de70e511644bd6df94
AlphaBaqpla <str14017355@gmail.com>
2023-01-10 21:47:34 +0300

    Update custom.js

---
+++ methods/custom.js
@@ -0,0 +1,8 @@
+//===============CUSTOM METHOD================
+module.exports = {
+    name: 'rcon',
+    async execute(proj,user,time,sign){
+        console.log('мой крутой код выводит в консоль сообщение да вот оно \n'+proj,user,time,sign)
+        //ваш собственный код
+    }
+}
