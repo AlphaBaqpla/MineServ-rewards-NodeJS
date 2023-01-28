93703ae0c2ab5238b5cfb3ebb3392e52a99983f5
David Greitan <str14017355@gmail.com>
2023-01-28 22:53:58 +0300

    Update custom.js

--- methods/custom.js
+++ methods/custom.js
@@ -1,6 +1,6 @@
 //===============CUSTOM METHOD================
 module.exports = {
-    name: 'rcon',
+    name: 'Custom',
     async execute(proj,user,time,sign){
         console.log('мой крутой код выводит в консоль сообщение да вот оно \n'+proj,user,time,sign)
         //ваш собственный код
